b92bcc15cfccd1f7e94a26d6c5fe9aeca7abc5a2
hou27 <ataj125@gmail.com>
2023-09-20 00:30:21 +0900

    landmark를 덧씌운 이미지를 다시 보이도록 처리
    여러 이미지를 추가로 테스트하며 필요에 의해 주석 해제

--- blazepose_image/main.py
+++ blazepose_image/main.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-# from draw_landmarks_on_image import draw_landmarks_on_image
+from draw_landmarks_on_image import draw_landmarks_on_image
 
 # STEP 2: Create an PoseLandmarker object.
 base_options = python.BaseOptions(model_asset_path="./model/pose_landmarker.task")
@@ -15,16 +15,21 @@ detector = vision.PoseLandmarker.create_from_options(options)
 image1 = "038-1-1-21-Z17_D-0000004.jpg"
 image2 = "506-1-2-23-Z57_A-0000009.jpg"
 image3 = "561-1-3-27-Z37_C-0000001.jpg"
+image4 = "test_converted.jpg"
+image5 = "stand_body_converted.jpg"
+image6 = "mingyu_stand_converted.jpg"
 # STEP 3: Load the input image.
 # image = mp.Image.create_from_file("image.jpg")
-image = mp.Image.create_from_file(image3)
+image = mp.Image.create_from_file(image3)  # jpg만 가능
 
 # STEP 4: Detect pose landmarks from the input image.
 detection_result = detector.detect(image)
 
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
 
-"""
-step 5부터 주석 처리 및 landmark 출력 코드로 변경
+print(detection_result.pose_landmarks)
+# """
+# step 5부터 주석 처리 및 landmark 출력 코드로 변경
 
 # STEP 5: Process the detection result. In this case, visualize it.
 annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
@@ -36,6 +41,4 @@ cv2.waitKey(0)
 
 # 모든 창을 닫습니다.
 cv2.destroyAllWindows()
-"""
-
-print(detection_result.pose_landmarks)
+# """
