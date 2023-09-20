3fc3980f6f2a3188127d9e0fd3d81bdad54a76b7
hou27 <ataj125@gmail.com>
2023-09-20 18:18:24 +0900

    image에서 landmark를 추출한 후 npy 파일로 저장

--- blazepose_image/main.py
+++ blazepose_image/main.py
@@ -3,6 +3,8 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import numpy as np
+import os
 
 from draw_landmarks_on_image import draw_landmarks_on_image
 
@@ -12,23 +14,52 @@ options = vision.PoseLandmarkerOptions(
     base_options=base_options, output_segmentation_masks=True
 )
 detector = vision.PoseLandmarker.create_from_options(options)
-image1 = "038-1-1-21-Z17_D-0000004.jpg"
-image2 = "506-1-2-23-Z57_A-0000009.jpg"
-image3 = "561-1-3-27-Z37_C-0000001.jpg"
-image4 = "test_converted.jpg"
-image5 = "stand_body_converted.jpg"
-image6 = "mingyu_stand_converted.jpg"
+
+# list for keypoint data
+landmarks_data = []
+
+# load img files
+image_directory = "./image/561-1-3-27-Z37_C"
+image_files = [f for f in os.listdir(image_directory) if f.endswith((".jpg", ".jpeg"))]
+
+# detect pose landmarks from the input image and save landmarks to npy file
+for image_file in image_files:
+    image_path = os.path.join(image_directory, image_file)
+    image = mp.Image.create_from_file(image_path)
+
+    # Detect pose landmarks from the input image.
+    detection_result = detector.detect(image)
+
+    # Process the detection result.
+    landmarks = [
+        (landmark.x, landmark.y, landmark.z)
+        for landmark in detection_result.pose_landmarks[0]
+    ]
+
+    # save landmarks to npy file
+    np.save(f"./npy/{image_file}.npy", np.array(landmarks))
+
+# image1 = "038-1-1-21-Z17_D-0000004.jpg"
+# image2 = "506-1-2-23-Z57_A-0000009.jpg"
+# image3 = "561-1-3-27-Z37_C-0000001.jpg"
+# image4 = "test_converted.jpg"
+# image5 = "stand_body_converted.jpg"
+# image6 = "mingyu_stand_converted.jpg"
+# image7 = "561-1-3-27-Z37_C-0000009.jpg"
+# image8 = "561-1-3-27-Z56_C-0000001.jpg"
+# image9 = "561-1-3-27-Z56_C-0000004.jpg"
 # STEP 3: Load the input image.
 # image = mp.Image.create_from_file("image.jpg")
-image = mp.Image.create_from_file(image3)  # jpg만 가능
+# image = mp.Image.create_from_file(image9)  # jpg만 가능
 
 # STEP 4: Detect pose landmarks from the input image.
-detection_result = detector.detect(image)
+# detection_result = detector.detect(image)
 
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+# annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
 
-print(detection_result.pose_landmarks)
-# """
+# print(detection_result.pose_landmarks)
+
+"""
 # step 5부터 주석 처리 및 landmark 출력 코드로 변경
 
 # STEP 5: Process the detection result. In this case, visualize it.
@@ -41,4 +72,4 @@ cv2.waitKey(0)
 
 # 모든 창을 닫습니다.
 cv2.destroyAllWindows()
-# """
+"""
