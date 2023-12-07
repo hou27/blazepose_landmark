41ff7f0c39bff3c144ded73448f1c84b3d367b12
hou27 <ataj125@gmail.com>
2023-12-07 20:30:32 +0900

    json.dumps로 감싸지 않고 json 자체를 반환하도록 수정

--- ml_server/pushupCountMachine.py
+++ ml_server/pushupCountMachine.py
@@ -24,7 +24,7 @@ class PushupCountMachine:
         elapsed_time = curr_time - self.start_time if self.start_time else 0
 
         if result is None:
-            return json.dumps({"count": self.cnt, "time": elapsed_time})
+            return {"count": self.cnt, "time": elapsed_time}
 
         print("curr status : ", result[0])
         self.curr_status = result[0]
@@ -36,7 +36,7 @@ class PushupCountMachine:
                 if self.cnt == 1:
                     self.start_time = curr_time
 
-        return json.dumps({"count": self.cnt, "time": elapsed_time})
+        return {"count": self.cnt, "time": elapsed_time}
 
 
 # class PushupCountMachine:
--- ml_server/squatCountMachine.py
+++ ml_server/squatCountMachine.py
@@ -23,7 +23,7 @@ class SquatCountMachine:
         elapsed_time = curr_time - self.start_time if self.start_time else 0
 
         if result is None:
-            return json.dumps({"count": self.cnt, "time": elapsed_time})
+            return {"count": self.cnt, "time": elapsed_time}
 
         print("curr status : ", result[0])
         self.curr_status = result[0]
@@ -35,4 +35,4 @@ class SquatCountMachine:
                 if self.cnt == 1:
                     self.start_time = curr_time
 
-        return json.dumps({"count": self.cnt, "time": elapsed_time})
+        return {"count": self.cnt, "time": elapsed_time}
